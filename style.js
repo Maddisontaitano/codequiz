d9b9ffc0f2044064dddc9783a14fdb26c7e6d655
Maddison Taitano <maddisontaitano@gmail.com>
2020-11-10 16:50:00 -0500

    Made button and started questions.

---
+++ style.js
@@ -0,0 +1,14 @@
+
+var preQuizWindow = document.querySelector(".preQuizWindow");
+var timer = document.getElementById("timer");
+var startButton = document.getElementById("startButton");
+
+var quizQuestions = document.querySelector(".quizQuestions");
+var quizQuestionsTitle = document.querySelector("#quizQuestionsTitle");
+
+var rightAnswer = document.querySelector(".rightAnswer");
+var wrongAnswer = document.querySelector(".wrongAnswer");
+var but1 = document.querySelector("#but1");
+var but2 = document.querySelector("#but2");
+var but3 = document.querySelector("#but3");
+var but4 = document.querySelector("#but4");
